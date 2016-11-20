d6ee64c440e29f7f466efb4228970e2def2ea678
Keren Megory-Cohen <keren.megorycohen@colorado.edu>
2016-11-19 17:07:44 -0700

    Initialize web template with nav and header

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,37 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import styles from './index.scss';
 import React from 'react';
-import Navbar from './navbar.jsx';
+import styles from './scss/index.scss';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-      <Navbar/>
+      <div className="mdl-layout mdl-js-layout">
+        <header className="mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            {/* Title */}
+            <span className="mdl-layout-title">Safe Spot</span>
+            {/* Add spacer, to align navigation to the right */}
+            <div className="mdl-layout-spacer" />
+            {/* Navigation */}
+            <nav className="mdl-navigation">
+              <a className="mdl-navigation__link" href>Map</a>
+              <a className="mdl-navigation__link" href>Feed</a>
+            </nav>
+          </div>
+        </header>
+        <div className="mdl-layout__drawer">
+          <span className="mdl-layout-title">Title</span>
+          <nav className="mdl-navigation">
+            <a className="mdl-navigation__link" href>Map</a>
+            <a className="mdl-navigation__link" href>Feed</a>
+          </nav>
+        </div>
+          <main className="mdl-layout__content">
+            <div className={styles.logoHeader}></div>
+            <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Button
+            </button>
+          </main>
       </div>
+
     )
   }
 }
